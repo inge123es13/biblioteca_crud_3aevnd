3c226b0a234a4ea3f90e3145524701631442536b
inge123es13 <ingridestefanyp@gmail.com>
2026-06-08 12:44:10 -0600

    modelos de libro, usuario y prestamo

---
+++ models/libro.py
@@ -0,0 +1,28 @@
+class Libro:
+
+    # Metodo constructor
+    def __init__(self,id , titulo, autor, isbn):
+        self.id=id
+        self.titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+        self.disponible = True
+
+    # Mostrar en la pantalla la información de unS libro
+    def mostrar_info(self):
+
+        estado = "Disponible" if self.disponible else "Prestado"
+        return f"{self.titulo} - {self.autor}: {estado}"
+
+    def prestar(self):
+        if self.disponible:
+            self.disponible = False
+            print(f"**El libro {self.titulo} esta prestado")
+            return True
+        else:
+            print(f"El libro {self.titulo} no esta disponible")
+            return False
+
+    def devolver(self):
+        self.disponible = True
+        print(f"El libro {self.titulo} ha sido devuelto")
