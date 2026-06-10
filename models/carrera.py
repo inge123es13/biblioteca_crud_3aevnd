869a96d380ca9e04dfdb2352ed45e66cec469557
inge123es13 <ingridestefanyp@gmail.com>
2026-06-10 10:39:53 -0600

    Agregando modelos

---
+++ models/carrera.py
@@ -0,0 +1,8 @@
+class Carrera:
+
+    def __init__(self, id , carrera):
+        self.id = id
+        self.carrera = carrera
+
+    def mostrar_info(self):
+        return f"{self.id} - {self.carrera}"
