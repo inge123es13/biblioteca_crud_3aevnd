32631a90f8e18a39e31085a768acb33701946882
inge123es13 <ingridestefanyp@gmail.com>
2026-06-15 12:34:47 -0600

    LIbroDAO

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
- 
+from dao.libro_dao import LibroDAO
+from models.libro import Libro
+
+def main():
+    try:
+        libro_dao = LibroDAO()
+        libros= libro_dao.obtener_libros()
+
+        print("Libros en la biblioteca")
+        if len(libros) == 0:
+           print("No hay libros registrados")
+        else:
+            for libro in libros:
+                print(f"{libro.titulo} - {libro.autor} - {libro.disponible}")
+
+        print("\n Conexión exitosa a la base de datos")
+    except Exception as e:
+        print(f"Error al conectar la base de datos: {e}")
+
+if __name__ == "__main__":
+    main()
