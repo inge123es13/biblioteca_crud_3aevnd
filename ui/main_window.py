0e4de2fbc2bb941720901dc8a7d9c3a8ff0bb59e
inge123es13 <ingridestefanyp@gmail.com>
2026-07-12 17:14:03 -0600

    Biblioteca

--- ui/main_window.py
+++ ui/main_window.py
@@ -3,92 +3,88 @@ import flet as ft
 
 def main_window(page: ft.Page):
 
-    page.titulo ("Sistema de gestión de Biblioteca")
-    page.window_width = 1100
-    page.window_height= 700
+    page.title = "Sistema de gestión de Biblioteca"
+    page.window.width = 1100
+    page.window.height = 700
     page.padding = 0
     page.bgcolor = ft.Colors.BLUE_GREY_50
 
-    #Etiqueta o texto
     titulo = ft.Text(
         "Sistema de Gestión de Biblioteca",
         size=24,
-        weight = ft.Weight.BOLD
+        weight=ft.FontWeight.BOLD
     )
 
     subtitulo = ft.Text(
         "Seleccione una opción del menú",
-        size= 16,
-        color= ft.Colors.BLUE_GREY_500
+        size=16,
+        color=ft.Colors.BLUE_GREY_500
     )
 
-    #CONTENEDOR CENTRAL
     contenido = ft.Container(
-        content= ft.Column(
-            controls= [
+        content=ft.Column(
+            controls=[
                 titulo,
                 subtitulo
             ],
-            spacing= 10
+            spacing=10
         ),
-        padding= 30,
-        expand= True
+        padding=30,
+        expand=True
     )
-    
 
-    #menu lateral
-    menu_lateral= ft.Container(
-        width= 220,
-        bgcolor= ft.Colors.BLUE_GREY_900,
+    menu_lateral = ft.Container(
+        width=220,
+        bgcolor=ft.Colors.BLUE_GREY_900,
         padding=20,
-        content= ft.Column(
-            controls= [
-                ft.text(
+        content=ft.Column(
+            controls=[
+                ft.Text(
                     "Biblioteca",
-                    size =22,
-                    weight = ft.FontWeight.BOLD,
-                    color= ft.Colors.WHITE
+                    size=22,
+                    weight=ft.FontWeight.BOLD,
+                    color=ft.Colors.WHITE
                 ),
                 ft.Text(
-                    "Sistema De Gestión",
+                    "Sistema de Gestión",
                     size=12,
-                    color= ft.Colors.BLUE_GREY_100
+                    color=ft.Colors.BLUE_GREY_100
                 ),
-                ft.Divider(color = ft.Colors.BLUE_GREY_700),
-                #botones
+                ft.Divider(
+                    color=ft.Colors.BLUE_GREY_700
+                ),
                 ft.ElevatedButton(
-                    text = "Libros",
-                    icon = ft.Icons.BOOK,
+                    content="Libros",
+                    icon=ft.Icons.BOOK,
                     width=180
                 ),
                 ft.ElevatedButton(
-                    text = "Usuarios",
-                    icon = ft.Icons.PERSON,
+                    content="Usuarios",
+                    icon=ft.Icons.PERSON,
                     width=180
                 ),
                 ft.ElevatedButton(
-                    text = "Prestamos",
-                    icon = ft.Icons.SWAP_HORIZ,
+                    content="Préstamos",
+                    icon=ft.Icons.SWAP_HORIZ,
                     width=180
                 ),
                 ft.ElevatedButton(
-                    text = "Devoluciones",
-                    icon = ft.Icons.KEYBOARD_RETURN,
+                    content="Devoluciones",
+                    icon=ft.Icons.KEYBOARD_RETURN,
                     width=180
                 ),
             ],
-            spacing= 15
+            spacing=15
         )
     )
 
-    #Definicion del Layout de la pagina
     layout = ft.Row(
-        controls= [
+        controls=[
             menu_lateral,
             contenido
         ],
-        expand= True
+        expand=True,
+        spacing=0
     )
 
-    #Agregar layout a la pagina 
     page.add(layout)
